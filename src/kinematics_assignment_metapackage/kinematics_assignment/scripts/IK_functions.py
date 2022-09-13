ee33d16339dff29bf8050f44d59684e0a92944a3
Jannik Wagner <jannik.wagner@proton.me>
2022-09-13 12:36:02 +0200

    feat/A2: Added scara - wip

--- src/kinematics_assignment_metapackage/kinematics_assignment/scripts/IK_functions.py
+++ src/kinematics_assignment_metapackage/kinematics_assignment/scripts/IK_functions.py
@@ -5,11 +5,32 @@
     # {wagne@kth.se}
 """
 
+import math
+
 def scara_IK(point):
     x = point[0]
     y = point[1]
     z = point[2]
-    q = [0.0, 0.0, z]
+
+    l0 = 0.07
+    l1 = 0.3
+    l2 = 0.35
+
+    q3 = z
+
+    x_ = x - l0
+
+    c2 = (x_**2 + y**2 - l1**2 - l2**2) / (2 * l1 * l2)
+    s2 = math.sqrt(1-c2**2) # add sign
+
+    q2 = math.atan2(s2, c2)
+
+    s1 = ((l1 + l2 * c2) * y - l2 * s2 * x_) / (x_**2 + y**2)
+    c1 = ((l1 + l2 * c2) * x_ - l2 * s2 * y) / (x_**2 + y**2)
+
+    q1 = math.atan2(s1, c1)
+
+    q = [q1, q2, q3]
 
     """
     Fill in your IK solution here and return the three joint values in q
